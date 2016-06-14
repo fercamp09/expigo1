69cd639ece6c40185acd6be071bf66b88e1bab2f
Fernando Campana <fercamp09@gmail.com>
2016-06-14 00:26:18 -0500

    Prueba de travis 5

---
+++ www/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:80/admin/');
+define('HTTP_CATALOG', 'http://localhost:80/');
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:80/admin/');
+define('HTTPS_CATALOG', 'http://localhost:80/');
+// DIR
+define('DIR_APPLICATION', 'C:/Users/Ecotel/expigo1/www/admin/');
+define('DIR_SYSTEM', 'C:/Users/Ecotel/expigo1/www/system/');
+define('DIR_DATABASE', 'C:/Users/Ecotel/expigo1/www/system/database/');
+define('DIR_LANGUAGE', 'C:/Users/Ecotel/expigo1/www/admin/language/');
+define('DIR_TEMPLATE', 'C:/Users/Ecotel/expigo1/www/admin/view/template/');
+define('DIR_CONFIG', 'C:/Users/Ecotel/expigo1/www/system/config/');
+define('DIR_IMAGE', 'C:/Users/Ecotel/expigo1/www/image/');
+define('DIR_CACHE', 'C:/Users/Ecotel/expigo1/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/Users/Ecotel/expigo1/www/system/storage/download/');
+define('DIR_UPLOAD', 'C:/Users/Ecotel/expigo1/www/system/storage/upload/');
+define('DIR_LOGS', 'C:/Users/Ecotel/expigo1/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/Users/Ecotel/expigo1/www/system/storage/modification/');
+define('DIR_CATALOG', 'C:/Users/Ecotel/expigo1/www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'fer');
+define('DB_PASSWORD', 'fer');
+define('DB_DATABASE', 'expigo');
+define('DB_PREFIX', 'oc_');
+define('DB_PORT', '3306');
+?>
---
+++ www/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:80/');
+define('HTTP_ADMIN', 'http://localhost:80/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:80/');
+// DIR
+define('DIR_APPLICATION', 'C:/Users/Ecotel/expigo1/www/catalog/');
+define('DIR_SYSTEM', 'C:/Users/Ecotel/expigo1/www/system/');
+define('DIR_DATABASE', 'C:/Users/Ecotel/expigo1/www/system/database/');
+define('DIR_LANGUAGE', 'C:/Users/Ecotel/expigo1/www/catalog/language/');
+define('DIR_TEMPLATE', 'C:/Users/Ecotel/expigo1/www/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/Users/Ecotel/expigo1/www/system/config/');
+define('DIR_IMAGE', 'C:/Users/Ecotel/expigo1/www/image/');
+define('DIR_CACHE', 'C:/Users/Ecotel/expigo1/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/Users/Ecotel/expigo1/www/system/storage/download/');
+define('DIR_UPLOAD', 'C:/Users/Ecotel/expigo1/www/system/storage/upload/');
+define('DIR_MODIFICATION', 'C:/Users/Ecotel/expigo1/www/system/storage/modification/');
+define('DIR_LOGS', 'C:/Users/Ecotel/expigo1/www/system/storage/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'fer');
+define('DB_PASSWORD', 'fer');
+define('DB_DATABASE', 'expigo');
+define('DB_PREFIX', 'oc_');
+define('DB_PORT', '3306');
+?>
